8dd61c9df75c0ac37e228b5354f2993d7a745dc0
David Lai <david.lai@evermight,com>
2023-06-05 11:26:12 -0400

    reading from my program

--- nodejs-mysql/index.js
+++ nodejs-mysql/index.js
@@ -5,18 +5,28 @@ async function myProgram() {
 	  host     : 'localhost',
 	  user     : 'root',
 	  password : '!!HomeWork!!0',
-	  database : 'world'
+	  database : 'myprogram'
 	});
 	 
 	connection.connect();
 	const dbresults = await new Promise((res,rej)=>{
-		connection.query('SELECT * FROM country', function (error, results, fields) {
+		
+		const query = `
+SELECT * FROM student_school_course
+INNER JOIN student ON student_school_course.student_id = student.student_id
+INNER JOIN course ON student_school_course.course_id = course.course_id
+INNER JOIN school ON student_school_course.school_id = school.school_id
+WHERE student.dob > 1970;
+		
+		`;
+		
+		connection.query(query, function (error, results, fields) {
 		  if (error) rej(error);
 		  else res(results);
 		});
 	});
 	console.log("try");
-	console.log(dbresults[0].Name);
+	console.log(dbresults[0]);
 	console.log("again");
 	connection.end();
 
